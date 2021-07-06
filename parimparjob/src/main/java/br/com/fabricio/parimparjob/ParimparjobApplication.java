0f6fe24259bde85c8782bd623e955b8c2c192194
fabricio-leitao <fabricio-beco@hotmail.com>
2021-07-06 19:25:47 -0300

    Tipos de Step, reader, processor e writer

---
+++ parimparjob/src/main/java/br/com/fabricio/parimparjob/ParimparjobApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabricio.parimparjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParimparjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParimparjobApplication.class, args);
+	}
+
+}
---
+++ parimparjob/src/test/java/br/com/fabricio/parimparjob/ParimparjobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabricio.parimparjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParimparjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
